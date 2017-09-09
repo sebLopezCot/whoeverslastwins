f1c210f1554acba23eb2b5c3a1ec16fae9b74882
tysonzero <danielspencersmith@gmail.com>
2017-09-09 05:01:31 -0400

    Minor cleanup

--- core/src/Main.hs
+++ core/src/Main.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -14,27 +13,27 @@ import GHC.Generics
 import Network.Wai.Handler.Warp
 import Servant
 
-data Player = Player {
-    id :: Int,
-    username :: String,
-    password :: String,
-    score :: Int
-} deriving (Eq, Show, Generic)
+data Player = Player
+    { id :: Int
+    , username :: String
+    , password :: String
+    , score :: Int
+    } deriving (Eq, Show, Generic)
 
 instance ToJSON Player
 
-data ClientWriteableInfo = ClientWriteableInfo {
-    inputUsername :: String,
-    inputPassword :: String
-} deriving (Eq, Show, Generic)
+data PlayerCreate = PlayerCreate
+    { inputUsername :: String
+    , inputPassword :: String
+    } deriving (Eq, Show, Generic)
 
-instance FromJSON ClientWriteableInfo
+instance FromJSON PlayerCreate
 
-type API = "users" :> ReqBody '[JSON] ClientWriteableInfo :> Post '[JSON] Player
-        :<|> "users" :> Get '[JSON] [Player]
-        :<|> "users" :> Capture "id" Int :> Get '[JSON] Player 
-        :<|> "users" :> Capture "id" Int :> ReqBody '[JSON] ClientWriteableInfo :> Patch '[JSON] ()
-        :<|> "users" :> Capture "id" Int :> Delete '[JSON] ()
+type API = "users" :> ReqBody '[JSON] PlayerCreate :> Post '[JSON] Player
+      :<|> "users" :> Get '[JSON] [Player]
+      :<|> "users" :> Capture "id" Int :> Get '[JSON] Player 
+      :<|> "users" :> Capture "id" Int :> ReqBody '[JSON] PlayerCreate :> Patch '[JSON] ()
+      :<|> "users" :> Capture "id" Int :> Delete '[JSON] ()
 
 server :: Server API
 server = createPlayer
@@ -42,17 +41,16 @@ server = createPlayer
     :<|> getPlayer
     :<|> updatePlayer
     :<|> deletePlayer
+  where
+    createPlayer info = pure (Player 0 "Seb" "p@ssword" 350)
 
-    where
-        createPlayer info = pure (Player 0 "Seb" "p@ssword" 350)
+    getAllPlayers = pure [Player 0 "Seb" "p@ssword" 350]
+    
+    getPlayer id_ = pure (Player 0 "Seb" "p@ssword" 350)
 
-        getAllPlayers = pure [(Player 0 "Seb" "p@ssword" 350)]
-        
-        getPlayer id_ = pure (Player 0 "Seb" "p@ssword" 350)
+    updatePlayer id_ info = pure ()
 
-        updatePlayer id_ info = pure ()
-
-        deletePlayer id_ = pure ()
+    deletePlayer id_ = pure ()
 
 api :: Proxy API
 api = Proxy
