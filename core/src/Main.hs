a8cb17bfbad62f0b85f28fa080259da71fa7c6b3
tysonzero <danielspencersmith@gmail.com>
2017-09-09 16:30:52 -0400

    Flesh out users endpoint

--- core/src/Main.hs
+++ core/src/Main.hs
@@ -4,61 +4,107 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeOperators #-}
 
 module Main (main) where
 
+import Control.Concurrent.MVar (MVar, modifyMVar, newMVar, readMVar)
+import Control.Monad.IO.Class (liftIO)
 import Data.Aeson.Types
-import GHC.Generics
-import Network.Wai.Handler.Warp
+    ( FromJSON, ToJSON, Value(Object), object
+    , parseJSON, toJSON, typeMismatch, (.:), (.=)
+    )
+import Data.Bool (bool)
+import Data.Foldable (find)
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Monoid ((<>))
+import GHC.Generics (Generic)
+import Network.Wai.Handler.Warp (run)
 import Servant
+    ( Application, Capture, Delete, Get, JSON, Patch, Post
+    , Proxy(Proxy), ReqBody, Server, serve, (:<|>)((:<|>)), (:>)
+    )
 
 data Player = Player
-    { id :: Int
+    { id_ :: Int
     , username :: String
     , password :: String
     , score :: Int
     } deriving (Eq, Show, Generic)
 
-instance ToJSON Player
+instance ToJSON Player where
+    toJSON pl = object
+        [ "id" .= id_ pl
+        , "username" .= username pl
+        , "password" .= password pl
+        , "score" .= score pl
+        ]
 
 data PlayerCreate = PlayerCreate
-    { inputUsername :: String
-    , inputPassword :: String
+    { createUsername :: String
+    , createPassword :: String
     } deriving (Eq, Show, Generic)
 
-instance FromJSON PlayerCreate
+instance FromJSON PlayerCreate where
+    parseJSON (Object v) = PlayerCreate
+        <$> v .: "username"
+        <*> v .: "password"
+    parseJSON invalid = typeMismatch "PlayerCreate" invalid
+
+data PlayerUpdate = PlayerUpdate
+    { updateUsername :: Maybe String
+    , updatePassword :: Maybe String
+    } deriving (Eq, Show, Generic)
+
+instance FromJSON PlayerUpdate where
+    parseJSON (Object v) = PlayerUpdate
+        <$> v .: "username"
+        <*> v .: "password"
+    parseJSON invalid = typeMismatch "PlayerUpdate" invalid
 
 type API = "users" :> ReqBody '[JSON] PlayerCreate :> Post '[JSON] Player
       :<|> "users" :> Get '[JSON] [Player]
       :<|> "users" :> Capture "id" Int :> Get '[JSON] Player 
-      :<|> "users" :> Capture "id" Int :> ReqBody '[JSON] PlayerCreate :> Patch '[JSON] ()
+      :<|> "users" :> Capture "id" Int :> ReqBody '[JSON] PlayerUpdate :> Patch '[JSON] ()
       :<|> "users" :> Capture "id" Int :> Delete '[JSON] ()
 
-server :: Server API
-server = createPlayer
-    :<|> getAllPlayers
-    :<|> getPlayer
-    :<|> updatePlayer
-    :<|> deletePlayer
+server :: MVar [Player] -> Server API
+server players = createPlayer
+            :<|> getAllPlayers
+            :<|> getPlayer
+            :<|> updatePlayer
+            :<|> deletePlayer
   where
-    createPlayer info = pure (Player 0 "Seb" "p@ssword" 350)
+    createPlayer pc = liftIO . modifyMVar players $ \ps -> do
+        let pl = Player
+                { id_ = succ . maximum . (0 :) $ id_ <$> ps
+                , username = createUsername pc
+                , password = createPassword pc
+                , score = 0
+                }
+        pure (pl : ps, pl)
 
-    getAllPlayers = pure [Player 0 "Seb" "p@ssword" 350]
-    
-    getPlayer id_ = pure (Player 0 "Seb" "p@ssword" 350)
+    getAllPlayers = liftIO $ readMVar players
 
-    updatePlayer id_ info = pure ()
+    getPlayer id' = liftIO $ do
+        ps <- readMVar players
+        pure . fromJust $ find ((== id') . id_) ps
 
-    deletePlayer id_ = pure ()
+    updatePlayer id' pu = liftIO . modifyMVar players $ \ps -> do
+        let update pl = flip (bool pl) (id' == id_ pl) $ pl
+                { username = fromMaybe (username pl) (updateUsername pu)
+                , password = fromMaybe (password pl) (updatePassword pu)
+                }
+        pure (update <$> ps, ())
 
-api :: Proxy API
-api = Proxy
+    deletePlayer id' = liftIO . modifyMVar players $ \ps ->
+        pure (take id' ps <> drop (id' + 1) ps, ())
 
-app :: Application
-app = serve api server
+app :: MVar [Player] -> Application
+app = serve @API Proxy . server
 
 main :: IO ()
-main = run 8081 app
-
-
+main = do
+    players <- newMVar []
+    run 8081 $ app players
