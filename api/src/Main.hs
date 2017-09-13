d38ca06a2c181b0d5c5cf57c3e24ac82676c3e25
tysonzero <danielspencersmith@gmail.com>
2017-09-13 03:27:51 -0400

    Split up core into separate api and core projects

--- api/src/Main.hs
+++ api/src/Main.hs
@@ -1,52 +1,20 @@
-{-# LANGUAGE DataKinds, FlexibleContexts, OverloadedStrings #-}
-{-# LANGUAGE TypeApplications, TypeFamilies, TypeOperators #-}
+{-# LANGUAGE DataKinds, OverloadedStrings, TypeApplications, TypeFamilies #-}
 
 module Main (main) where
 
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Logger (runNoLoggingT)
 import Control.Monad.Reader (ReaderT, runReaderT)
-import Data.Aeson.Types (FromJSON, Value(Object), parseJSON, typeMismatch, (.:))
 import Data.Maybe (catMaybes, fromJust)
 import Database.Persist (delete, entityVal, get, insert_, selectList, update, (=.))
 import Database.Persist.Sql (SqlBackend, runMigration)
 import Database.Persist.Sqlite (withSqliteConn)
 import Network.Wai.Handler.Warp (run)
-import Servant
-    ( Application, Capture, Delete, Get, JSON, Patch, Post
-    , Proxy(Proxy), ReqBody, ServerT, serve, (:<|>)((:<|>)), (:>)
-    )
+import Servant (Application, Proxy(Proxy), ServerT, serve, (:<|>)((:<|>)))
 import Servant.Utils.Enter ((:~>)(NT), enter)
 
+import Api
 import Models.User
-
-data UserCreate = UserCreate
-    { createUsername :: String
-    , createPassword :: String
-    } deriving (Eq, Show)
-
-instance FromJSON UserCreate where
-    parseJSON (Object v) = UserCreate
-        <$> v .: "username"
-        <*> v .: "password"
-    parseJSON invalid = typeMismatch "UserCreate" invalid
-
-data UserUpdate = UserUpdate
-    { updateUsername :: Maybe String
-    , updatePassword :: Maybe String
-    } deriving (Eq, Show)
-
-instance FromJSON UserUpdate where
-    parseJSON (Object v) = UserUpdate
-        <$> v .: "username"
-        <*> v .: "password"
-    parseJSON invalid = typeMismatch "UserUpdate" invalid
-
-type API = "users" :> ReqBody '[JSON] UserCreate :> Post '[JSON] User
-      :<|> "users" :> Get '[JSON] [User]
-      :<|> "users" :> Capture "id" UserId :> Get '[JSON] User 
-      :<|> "users" :> Capture "id" UserId :> ReqBody '[JSON] UserUpdate :> Patch '[JSON] ()
-      :<|> "users" :> Capture "id" UserId :> Delete '[JSON] ()
 
 server :: ServerT API (ReaderT SqlBackend IO)
 server = createUser
