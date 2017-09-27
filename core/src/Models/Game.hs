25cf7c4c8d66374cac96d7ca079ef57973762c46
tysonzero <danielspencersmith@gmail.com>
2017-09-27 04:37:37 -0400

    Create Models.Game

---
+++ core/src/Models/Game.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE ExistentialQuantification, GeneralizedNewtypeDeriving, MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings, QuasiQuotes, TemplateHaskell, TypeFamilies #-}
+
+module Models.Game
+    ( EntityField(GamePlayer1, GamePlayer2, GameTurn, GameTimeout)
+    , Game(Game, gamePlayer1, gamePlayer2, gameTurn, gameTimeout)
+    , GameId
+    , migrateGame
+    ) where
+
+import Data.Aeson.Types (ToJSON, object, toJSON, (.=))
+import Data.DateTime (DateTime)
+import Database.Persist (EntityField)
+import Database.Persist.TH
+    ( derivePersistField, mkMigrate, mkPersist, persistLowerCase, share, sqlSettings )
+
+import Models.User
+
+share [mkPersist sqlSettings, mkMigrate "migrateGame"] [persistLowerCase|
+Game
+    player1 UserId
+    player2 UserId
+    turn Int
+    timeout DateTime
+    deriving Show
+|]
