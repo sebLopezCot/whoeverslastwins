c34f31b7348eb2307a20a872a4fdffcb6f8718de
tysonzero <danielspencersmith@gmail.com>
2017-09-27 05:19:29 -0400

    Create Api.Games

---
+++ core/src/Api/Games.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE DataKinds, OverloadedStrings, TypeOperators #-}
+
+module Api.Games (GamesAPI, createPlayer1, createPlayer2) where
+
+import Data.Aeson.Types (FromJSON, Value(Object), parseJSON, typeMismatch, (.:))
+import Servant
+    ( Capture, Delete, Get, JSON, Patch, Post
+    , ReqBody, (:<|>), (:>)
+    )
+
+import Models.Game
+import Models.User
+
+data GameCreate = GameCreate
+    { createPlayer1 :: UserId
+    , createPlayer2 :: UserId
+    } deriving (Eq, Show)
+
+instance FromJSON GameCreate where
+    parseJSON (Object v) = GameCreate
+        <$> v .: "player1"
+        <*> v .: "player2"
+    parseJSON invalid = typeMismatch "GameCreate" invalid
+
+type GamesAPI
+       = "games" :> ReqBody '[JSON] GameCreate :> Post '[JSON] Game
+    :<|> "games" :> Get '[JSON] [Game]
+    :<|> "games" :> Capture "id" GameId :> Get '[JSON] Game
